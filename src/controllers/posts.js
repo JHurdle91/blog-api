d532541c87533df377080d80139c0657b6504794
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-08-08 01:17:19 -0400

    add '/' route to posts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,3 +1,15 @@
+var Post = require("../models/post");
+
+exports.index = {
+  get: (req, res) => {
+    /* send list of all posts */
+    Post.find(callback).exec((err, list_posts) => {
+      if (err) return next(err);
+      res.json(list_posts);
+    });
+  },
+};
+
 exports.create = {
   post: (req, res) => {
     res.send("Not implemented: add post to db");
